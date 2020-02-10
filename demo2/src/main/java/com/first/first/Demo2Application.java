eaebe0395c324bb899dccb04d4ab91a7dbe08a83
abcdefg <choiyurim@naver.com>
2020-02-11 07:23:59 +0900

    web application으로 설정안한다.

--- demo2/src/main/java/com/first/first/Demo2Application.java
+++ demo2/src/main/java/com/first/first/Demo2Application.java
@@ -1,13 +1,22 @@
 package com.first.first;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
+//@SpringBootApplication
 public class Demo2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+
+		SpringApplication application = new SpringApplication(Demo2Application.class);
+		application.setWebApplicationType(WebApplicationType.NONE);
+		application.run(args);
+		
+		// SpringApplication.run(Demo2Application.class, args);
 	}
 
 }
