dc208f1c049a21bb3d90cc290c8c795d9ea674b5
abcdefg <choiyurim@naver.com>
2020-02-11 07:26:39 +0900

    //@EnableAutoConfiguration 은 web application으로 만들려고함 		//@Configuration 		//@ComponentScan 		//@EnableAutoConfiguration 		// 이 3가지를 다 사용하는것이 @SpringBootApplication

--- demo2/src/main/java/com/first/first/Demo2Application.java
+++ demo2/src/main/java/com/first/first/Demo2Application.java
@@ -2,20 +2,30 @@ package com.first.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
+@EnableAutoConfiguration
+
 //@SpringBootApplication
 public class Demo2Application {
 
 	public static void main(String[] args) {
 
+		//@EnableAutoConfiguration 은 web application으로 만들려고함
+		//@Configuration
+		//@ComponentScan
+		//@EnableAutoConfiguration
+		// 이 3가지를 다 사용하는것이 @SpringBootApplication
+		
+		
 		SpringApplication application = new SpringApplication(Demo2Application.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
-		
+
 		// SpringApplication.run(Demo2Application.class, args);
 	}
 
