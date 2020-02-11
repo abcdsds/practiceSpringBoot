0540b215276dd8dc40975bb636de8725e2ba594d
abcdefg <choiyurim@naver.com>
2020-02-11 11:40:55 +0900

    프로퍼티즈에서 값가져오기

--- demo2/src/main/java/com/first/first/Demo2Application.java
+++ demo2/src/main/java/com/first/first/Demo2Application.java
@@ -2,15 +2,14 @@ package com.first.first;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+//@Configuration
+//@ComponentScan
+//@EnableAutoConfiguration
 
-//@SpringBootApplication
+@SpringBootApplication
 public class Demo2Application {
 
 	public static void main(String[] args) {
@@ -23,12 +22,27 @@ public class Demo2Application {
 		//@EnableAutoConfiguration autoconfigure 안에
 		// org.springframework.boot.autoconfigure.EnableAutoConfiguration
 		// 으로 키값을 설정함
+		// 다 적용시키는건아니고 @conditional로 적용여부를 체크함
 		
 		SpringApplication application = new SpringApplication(Demo2Application.class);
 		application.setWebApplicationType(WebApplicationType.NONE);
 		application.run(args);
 
 		// SpringApplication.run(Demo2Application.class, args);
+	}
+	
+	
+	// 2.1버전 이후에는 에러남
+	// The bean 'holoman', defined in com.first.first.Demo21Application, could not be registered. A bean with that name has already been defined in class path resource [com/first/first/HolomanConfiguration.class] and overriding is disabled.
+	// Consider renaming one of the beans or enabling overriding by setting spring.main.allow-bean-definition-overriding=true
+	//
+	
+	@Bean
+	public Holoman holeman() {
+		Holoman holoman= new Holoman();
+		holoman.setHowLong(60);
+		holoman.setName("ds");
+		return holoman;
 	}
 
 }
---
+++ demo2/src/main/java/com/first/first/Holoman.java
@@ -0,0 +1,33 @@
+package com.first.first;
+
+public class Holoman {
+
+	String name;
+
+	int howLong;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getHowLong() {
+		return howLong;
+	}
+
+	public void setHowLong(int howLong) {
+		this.howLong = howLong;
+	}
+
+
+	public String toString() {
+		return "holoman [name=" + name + ", howLong=" + howLong + "]";
+	}
+
+	
+	
+	
+}
