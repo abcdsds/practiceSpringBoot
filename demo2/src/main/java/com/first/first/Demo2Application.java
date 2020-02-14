a6069181dd6ad1e93c5bd645a1781af606e7be6e
abcdefg <choiyurim@naver.com>
2020-02-14 12:54:39 +0900

    커넥터 추가, 원래 ssl설정한 8443 https커넥터 말고 8080새로만든 커넥터

--- demo2/src/main/java/com/first/first/Demo2Application.java
+++ demo2/src/main/java/com/first/first/Demo2Application.java
@@ -1,11 +1,15 @@
 package com.first.first;
 
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 
 //@Configuration
 //@ComponentScan
@@ -51,6 +55,31 @@ public class Demo2Application {
 		
 		/////////////////////////////////////////////////////////////////////////
 	}
+	
+	@GetMapping("/hello")
+	public String test() {
+		return "hello Spring!";
+	}
+	
+	@Bean
+	public ServletWebServerFactory serverFactory() {
+		
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+				
+	}
+	
+	private Connector createStandardConnector() {
+		
+		//새로운 커넥터로 8443포트의 원래 커넥터와는 다른 커넥터
+		
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080);
+		
+		return connector;
+	}
+	
 	
 	
 	// 2.1버전 이후에는 에러남
